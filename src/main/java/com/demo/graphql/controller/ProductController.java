159b9c7cebbc96ae4e24fa9904a73b4ee999324a
Dinesh Pabbi <dineshpabbi10@gmail.com>
2021-10-16 01:51:36 -0700

    Add readme

--- src/main/java/com/demo/graphql/controller/ProductController.java
+++ src/main/java/com/demo/graphql/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// Demo controller to show rest endpoint for same data
 @RestController
 public class ProductController {
     @Autowired
